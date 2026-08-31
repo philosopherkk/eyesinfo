fe256594c56926cb5da70706d8694b735dd664b4
philosopherkk <philosopherkk@gmail.com>
2026-08-31 16:55:13 +0800

    Add Dr Poon v1.8 mark and name-card assembler

---
+++ public/dr-poon-card-assemble.js
@@ -0,0 +1,4 @@
+window.DRPOON_PHOTOS=window.DRPOON_PHOTOS||{};
+if(window.DRPOON_CARD_PARTS&&window.DRPOON_CARD_PARTS.length){
+  window.DRPOON_PHOTOS.card="data:image/jpeg;base64,"+window.DRPOON_CARD_PARTS.join("");
+}
